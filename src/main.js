e8ae53a846e51aba39a4e3b7c3b5ab285af96e63
Vasiliy <vaskaroman@gmail.com>
2016-11-29 16:27:13 +0300

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,36 +1,57 @@
 var graphics = require('./graphics.js');
 var logic = require('./logic.js');
-graphics.loadGraphics(7,6);
-var field=logic.generateField(7,6);
-var players = [{'name':'Red', 'color':'red', 'value':-1}, {'name':'Yellow', 'color':'yellow', 'value':1}];
-var player = 0;
-var cells = document.getElementById('cells').children;
-for(var i=0;i<cells.length;i++){
-    cells[i].addEventListener('mouseover', mouseOver);
-    cells[i].addEventListener('click', click);
+
+var options = {
+    "horizontal": 7,
+    "vertical": 6,
+    "players": [{'name':'Red', 'color':'red'}, {'name':'Yellow', 'color':'yellow'}]
 };
-document.getElementById('restart').addEventListener('click', restart);
-logic.setInitialField(cells, 6);
+var playerValues = [-1,1];
+var cells = document.getElementById('cells').children;
+
+//load field, cells
+graphics.loadGraphics(options.horizontal,options.vertical);
+
+//generate field array
+var field=logic.generateField(options.horizontal,options.vertical);
+//add events
+addEventListeners();
+
+//set initial field state
+var player = 0;
+logic.setInitialField(cells, options.horizontal-1);
+
+//events
 function mouseOver(event){
     if(event.target.attributes.busy.value=='free'){
         event.target.className = 'available cell';
     }
 }
+
 function click(event){
     if(event.target.attributes.busy.value=='free'){
-        event.target.className = players[player].color + ' cell';
-        event.target.setAttribute('busy', players[player].color);
-        event.target.setAttribute('player', player);
-        field[event.target.attributes.ver.value-1][event.target.attributes.hor.value-1] = players[player].value;
-        if(logic.checkField(field, players, event.target.attributes)){
-            document.getElementById('content').innerHTML = 'Player '+players[player].name+' wins!';
+        event.target.className = options.players[player].color + ' cell';
+        event.target.setAttribute('busy', options.players[player].color);
+        event.target.setAttribute('player', playerValues[player]);
+        field[event.target.attributes.ver.value-1][event.target.attributes.hor.value-1] = playerValues[player];
+        if(logic.checkField(field, options.players, event.target.attributes)){
+            document.getElementById('content').innerHTML = 'Player '+options.players[player].name+' wins!';
         }else{
             player = logic.changePlayer(player);
             logic.updateField(event.target, cells);
         }
     }
 }
+
 function restart(){
     player = 0;
     logic.setInitialField(cells, 6);
+}
+//functions
+function addEventListeners(){
+    for(var i=0;i<cells.length;i++){
+        cells[i].addEventListener('mouseover', mouseOver);
+        cells[i].addEventListener('click', click);
+    };
+    document.getElementById('restart').addEventListener('click', restart);
 }
